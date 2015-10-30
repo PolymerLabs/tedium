7dc48246f77370e5b63965eff0e1c9bbc09971ba
Peter Burns <rictic@google.com>
2015-10-30 15:36:05 -0700

    Cleanup code for wider consumption.
    - added docs
    - broke cleanup passes out from the core logic
    - made jshint happy

---
+++ cleanup-passes.js
@@ -0,0 +1,293 @@
+/**
+ * @license
+ * Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+'use strict';
+
+const fs = require('fs');
+const hydrolysis = require('hydrolysis');
+const nodegit = require('nodegit');
+const path = require('path');
+
+
+/**
+ * The meat of the implementation. If any cleanup step makes any changes it
+ * should then set element.dirty. The rest of the implementation
+ * will take care of pushing the changes up and making PRs.
+ *
+ * Each cleanup step is given an object that contains a ton of info about an
+ * element repo.
+ *
+ * Properties of element:
+ *   dir: a string, like 'repos/paper-input' that's a relative path to a
+ *       directory that contains a pristine checkout of the element as it
+ *       exists at master.
+ *   ghRepo: metadata about the elements' github repo.
+ *       docs: https://developer.github.com/v3/repos/#get
+ *   repo: a nodegit Repository object. Useful for modifying the on-disk
+ *       git repo
+ *   dirty: set this to true if you make changes to the repo that need to be
+ *       pushed.
+ *   needsReview: set this to true if your changes need human review.
+ *       if true, all changes made to the element will go out into a PR that
+ *       will be assigned to you. otherwise the changes will be pushed directly
+ *       to master. has no effect if dirty is false.
+ *
+ * To add a cleanup step, just add it to this promise chain.
+ */
+function cleanup(element) {
+  return Promise.resolve()
+      .then(cleanupBower.bind(null, element))
+      .then(generateReadme.bind(null, element))
+      .then(generateContributionGuide.bind(null, element))
+      ;
+}
+
+/**
+ * Cleans up a number of common bower problems, like no "main" attribute,
+ * "main" being an array rather than a string, etc.
+ */
+function cleanupBower(element) {
+  // Write the bower config object out to the given path
+  function writeToBower(bowerPath, bowerConfig) {
+    fs.writeFileSync(bowerPath,
+        JSON.stringify(bowerConfig, null, 2) + '\n', 'utf8');
+  }
+
+  return Promise.resolve().then(() => {
+    const bowerPath = path.join(element.dir, 'bower.json');
+    if (!existsSync(bowerPath)) {
+      throw new Error('no bower.json');
+    }
+    const bowerConfig = JSON.parse(fs.readFileSync(bowerPath, 'utf8'));
+
+    // Clean up nonexistant bower file
+    if (!bowerConfig.main || bowerConfig.main.length === 0) {
+      const elemFile = path.basename(element.dir) + '.html';
+
+      if (existsSync(path.join(element.dir, elemFile))) {
+        bowerConfig.main = elemFile;
+        writeToBower(bowerPath, bowerConfig);
+        element.dirty = true;
+        return element.repo.createCommitOnHead(
+            ['bower.json'], getSignature(), getSignature(),
+            'Add bower main file.').then(() => element);
+      }
+      return null; // couldn't generate a bower main :(
+    }
+
+    // Clean up an array bower file:
+    if (Array.isArray(bowerConfig.main) && bowerConfig.main.length === 1) {
+      bowerConfig.main = bowerConfig.main[0];
+      writeToBower(bowerPath, bowerConfig);
+      element.dirty = true;
+      return element.repo.createCommitOnHead(
+          ['bower.json'], getSignature(), getSignature(),
+          'Convert bower main from array to string.').then(() => element);
+    }
+
+  });
+}
+
+/**
+ * Generates README.md for the element, unless it's in the blacklist.
+ */
+function generateReadme(element) {
+  const manualReadmeRepos = new Set([
+    'repos/molecules',
+    'repos/iron-elements',
+    'repos/paper-elements',
+    'repos/neon-elements',
+    'repos/gold-elements',
+    'repos/platinum-elements',
+    'repos/seed-element',
+    'repos/app-layout-templates',
+    'repos/polymer-starter-kit',
+    'repos/quick-element',
+    'repos/font-roboto',
+    'repos/iron-test-helpers',
+    'repos/font-roboto-local',
+
+    // Temporary:
+    // Blocked on https://github.com/Polymer/hydrolysis/issues/188
+    'repos/iron-iconset',
+    // Blocked on getting the order of elements more correct, and moving the
+    // extra documentation that's currently only in the README somewhere that
+    // tedium can access.
+    'repos/platinum-sw',
+    'repos/neon-animation',
+  ]);
+  if (manualReadmeRepos.has(element.dir)) {
+    return Promise.resolve();
+  }
+  const implementationFiles = [];
+  return Promise.resolve().then(() => {
+    const promises = [];
+    const allFiles = fs.readdirSync(element.dir);
+    for (const filename of allFiles) {
+      const fullPath = path.join(element.dir, filename);
+      if (/\.html$/.test(filename) && !/(index|demo)/.test(filename)) {
+        implementationFiles.push(filename);
+        promises.push(
+          hydrolysis.Analyzer.analyze(fullPath)
+            .then((analyzer) => ({filename, analyzer}))
+        );
+      }
+    }
+    return Promise.all(promises);
+  }).then((results) => {
+    const elementsByTagName = {};
+    const behaviorsByName = {};
+    for (const result of results) {
+      for (const element of result.analyzer.elements) {
+        elementsByTagName[element.is] = element;
+      }
+      for (const behavior of result.analyzer.behaviors) {
+        behaviorsByName[behavior.is] = behavior;
+      }
+    }
+    let readmeContents = `
+<!---
+
+This README is automatically generated from the comments in these files:
+${implementationFiles.join('  ')}
+
+Edit those files, and our readme bot will duplicate them over here!
+Edit this file, and the bot will squash your changes :)
+
+-->
+
+`;
+
+    if (existsSync(path.join(element.dir, '.travis.yml'))) {
+      readmeContents +=
+        `[![Build Status](https://travis-ci.org/${element.ghRepo.owner.login}/${element.ghRepo.name}.svg?branch=master)](https://travis-ci.org/${element.ghRepo.owner.login}/${element.ghRepo.name})\n\n`;
+    }
+
+    // These elements are going to have a page in the element catalog.
+    if (/^(gold|platinum|paper|neon|iron|carbon)-/.test(element.ghRepo.name)) {
+      readmeContents += `_[Demo and API Docs](https://elements.polymer-project.org/elements/${element.ghRepo.name})_` + '\n\n';
+    }
+
+    const tagNames = Object.keys(elementsByTagName);
+    // Sort elements alphabetically, except that the element that the repository
+    // is named after should come first.
+    tagNames.sort((l, r) => {
+      if (l === element.ghRepo.name) {
+        return -1;
+      }
+      if (r === element.ghRepo.name) {
+        return 1;
+      }
+      return l.localeCompare(r);
+    });
+
+    for (const tagName of tagNames) {
+      const analyzedElement = elementsByTagName[tagName];
+
+      if (analyzedElement.desc.trim() === '') {
+        readmeContents += `\n<!-- No docs for <${tagName}> found. -->\n`;
+        continue;
+      }
+
+      readmeContents += `
+##&lt;${tagName}&gt;
+
+${analyzedElement.desc}
+`;
+    }
+    for (const name in behaviorsByName) {
+      const behavior = behaviorsByName[name];
+
+      if (behavior.desc.trim() === '') {
+        readmeContents += `\n<!-- No docs for ${name} found. -->\n`;
+        continue;
+      }
+
+      readmeContents += `
+##${name}
+
+${behavior.desc}
+`;
+    }
+
+    const readmePath = path.join(element.dir, 'README.md');
+    const oldContents = fs.readFileSync(readmePath, 'utf8');
+    if (oldContents !== readmeContents) {
+      fs.writeFileSync(readmePath, readmeContents, 'utf8');
+      element.dirty = true;
+      return element.repo.createCommitOnHead(
+            ['README.md'], getSignature(), getSignature(),
+            'Autogenerate README file.');
+    }
+  });
+}
+
+
+let contributionGuideContents = null;
+/**
+ * Generates the CONTRIBUTING.md file for the element.
+ */
+function generateContributionGuide(element) {
+  const pathToCanonicalGuide = 'repos/ContributionGuide/CONTRIBUTING.md';
+  if (!existsSync(pathToCanonicalGuide)) {
+    return Promise.reject(new Error(
+        'Couldn\'t find canonical contribution guide. git checkout error?'));
+  }
+  if (!contributionGuideContents) {
+    contributionGuideContents = `
+<!--
+This file is autogenerated based on
+https://github.com/PolymerElements/ContributionGuide/blob/master/CONTRIBUTING.md
+
+If you edit that file, it will get updated everywhere else.
+If you edit this file, your changes will get overridden :)
+-->
+`;
+    contributionGuideContents += fs.readFileSync(pathToCanonicalGuide, 'utf8');
+  }
+  let existingGuideContents = '';
+  const pathToExistingGuide = path.join(element.dir, 'CONTRIBUTING.md');
+  let guideExists = false;
+  if (existsSync(pathToExistingGuide)) {
+    guideExists = true;
+    existingGuideContents = fs.readFileSync(pathToExistingGuide, 'utf8');
+  }
+  if (contributionGuideContents === existingGuideContents) {
+    return Promise.resolve();
+  }
+  fs.writeFileSync(pathToExistingGuide, contributionGuideContents, 'utf8');
+  element.dirty = true;
+  let commitMessage = 'Update contribution guide';
+  if (!guideExists) {
+    commitMessage = 'Create contribution guide';
+  }
+  return element.repo.createCommitOnHead(
+        ['CONTRIBUTING.md'], getSignature(), getSignature(),
+        commitMessage);
+}
+
+// Generates a git commit signature for the bot.
+function getSignature() {
+  return nodegit.Signature.now(
+      'Polymer Format Bot', 'format-bot@polymer-project.org');
+}
+
+function existsSync(fn) {
+  try {
+    fs.statSync(fn);
+    return true;
+  } catch(_) {
+    return false;
+  }
+}
+
+
+module.exports = cleanup;
+module.exports.existsSync = existsSync;
--- tedium.js
+++ tedium.js
@@ -1,14 +1,33 @@
+/**
+ * @license
+ * Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+/**
+ * This file contains the main flow for the bot. i.e. discovering and checking
+ * out repos, noticing if they've changed, and pushing those changes back to
+ * github if needed.
+ *
+ * The actual changes that the bot does are in cleanup-passes.
+ */
+
+
 'use strict';
 
 const fs = require('fs');
 const GitHubApi = require("github-cache");
 const promisify = require("promisify-node");
 const nodegit = require('nodegit');
-const clone = require("nodegit").Clone.clone;
 const path = require('path');
 const ProgressBar = require('progress');
 const rimraf = require('rimraf');
-const hydrolysis = require('hydrolysis');
+const cleanup = require('./cleanup-passes');
+
 
 let GITHUB_TOKEN;
 
@@ -22,23 +41,12 @@ The token only needs the 'public repos' permission.
 
 Generate a token here:   https://github.com/settings/tokens
 `
-  )
+  );
   process.exit(1);
 }
 
-function getSignature() {
-  return nodegit.Signature.now(
-      'Polymer Format Bot', 'format-bot@polymer-project.org');
-}
+const github = connectToGithub();
 
-function existsSync(fn) {
-  try {
-    fs.statSync(fn);
-    return true;
-  } catch(_) {
-    return false;
-  }
-}
 
 function getRepos() {
   const per_page = 100;
@@ -59,11 +67,6 @@ function getRepos() {
   });
 }
 
-
-function writeToBower(bowerPath, bowerConfig) {
-  fs.writeFileSync(bowerPath, JSON.stringify(bowerConfig, null, 2) + '\n', 'utf8');
-}
-
 function promiseAllWithProgress(promises, label) {
   const progressBar = new ProgressBar(`${label} [:bar] :percent`, {
     total: promises.length});
@@ -73,230 +76,12 @@ function promiseAllWithProgress(promises, label) {
   return Promise.all(promises);
 }
 
-// The meat of the implementation. If any cleanup step makes any changes it
-// should then set element.dirty. The rest of the implementation
-// will take care of pushing the changes up and making PRs.
-function cleanup(element) {
-  return Promise.resolve()
-      .then(cleanupBower.bind(null, element))
-      .then(cleanupReadme.bind(null, element))
-      .then(cleanupContributionGuide.bind(null, element))
-}
-
-function cleanupBower(element) {
-  return Promise.resolve().then(() => {
-    const bowerPath = path.join(element.dir, 'bower.json');
-    if (!existsSync(bowerPath)) {
-      throw new Error('no bower.json');
-    }
-    const bowerConfig = JSON.parse(fs.readFileSync(bowerPath, 'utf8'));
-
-    // Clean up nonexistant bower file
-    if (!bowerConfig.main || bowerConfig.main.length === 0) {
-      var elemFile = path.basename(element.dir) + '.html';
-
-      if (existsSync(path.join(element.dir, elemFile))) {
-        bowerConfig.main = elemFile;
-        writeToBower(bowerPath, bowerConfig);
-        element.dirty = true;
-        return element.repo.createCommitOnHead(
-            ['bower.json'], getSignature(), getSignature(),
-            'Add bower main file.').then(() => element);
-      }
-      return null; // couldn't generate a bower main :(
-    }
-
-    // Clean up an array bower file:
-    if (Array.isArray(bowerConfig.main) && bowerConfig.main.length === 1) {
-      bowerConfig.main = bowerConfig.main[0];
-      writeToBower(bowerPath, bowerConfig);
-      element.dirty = true;
-      return element.repo.createCommitOnHead(
-          ['bower.json'], getSignature(), getSignature(),
-          'Convert bower main from array to string.').then(() => element);
-    }
-
-  });
-}
-
-function cleanupReadme(element) {
-  const manualReadmeRepos = new Set([
-    'repos/molecules',
-    'repos/iron-elements',
-    'repos/paper-elements',
-    'repos/neon-elements',
-    'repos/gold-elements',
-    'repos/platinum-elements',
-    'repos/seed-element',
-    'repos/app-layout-templates',
-    'repos/polymer-starter-kit',
-    'repos/quick-element',
-    'repos/font-roboto',
-    'repos/iron-test-helpers',
-    'repos/font-roboto-local',
-
-    // Temporary:
-    // Blocked on https://github.com/Polymer/hydrolysis/issues/188
-    'repos/iron-iconset',
-    // Blocked on getting the order of elements more correct, and moving the
-    // extra documentation that's currently only in the README somewhere that
-    // tedium can access.
-    'repos/platinum-sw',
-    'repos/neon-animation',
-  ]);
-  if (manualReadmeRepos.has(element.dir)) {
-    return Promise.resolve();
-  }
-  const implementationFiles = [];
-  return Promise.resolve().then(() => {
-    const promises = [];
-    const allFiles = fs.readdirSync(element.dir);
-    for (const filename of allFiles) {
-      const fullPath = path.join(element.dir, filename);
-      if (/\.html$/.test(filename) && !/(index|demo)/.test(filename)) {
-        implementationFiles.push(filename);
-        promises.push(
-          hydrolysis.Analyzer.analyze(fullPath)
-            .then((analyzer) => ({filename, analyzer}))
-        );
-      }
-    }
-    return Promise.all(promises);
-  }).then((results) => {
-    const elementsByTagName = {};
-    const behaviorsByName = {};
-    for (const result of results) {
-      for (const element of result.analyzer.elements) {
-        elementsByTagName[element.is] = element;
-      }
-      for (const behavior of result.analyzer.behaviors) {
-        behaviorsByName[behavior.is] = behavior;
-      }
-    }
-    let readmeContents = `
-<!---
-
-This README is automatically generated from the comments in these files:
-${implementationFiles.join('  ')}
-
-Edit those files, and our readme bot will duplicate them over here!
-Edit this file, and the bot will squash your changes :)
-
--->
-
-`
-
-    if (existsSync(path.join(element.dir, '.travis.yml'))) {
-      readmeContents +=
-        `[![Build Status](https://travis-ci.org/${element.ghRepo.owner.login}/${element.ghRepo.name}.svg?branch=master)](https://travis-ci.org/${element.ghRepo.owner.login}/${element.ghRepo.name})\n\n`
-    }
-
-    // These elements are going to have a page in the element catalog.
-    if (/^(gold|platinum|paper|neon|iron|carbon)-/.test(element.ghRepo.name)) {
-      readmeContents += `_[Demo and API Docs](https://elements.polymer-project.org/elements/${element.ghRepo.name})_` + '\n\n';
-    }
-
-    var tagNames = Object.keys(elementsByTagName);
-    // Sort elements alphabetically, except that the element that the repository
-    // is named after should come first.
-    tagNames.sort((l, r) => {
-      if (l === element.ghRepo.name) {
-        return -1;
-      }
-      if (r === element.ghRepo.name) {
-        return 1;
-      }
-      return l.localeCompare(r);
-    })
-
-    for (const tagName of tagNames) {
-      const element = elementsByTagName[tagName];
-
-      if (element.desc.trim() === '') {
-        readmeContents += `\n<!-- No docs for <${tagName}> found. -->\n`;
-        continue;
-      }
-
-      readmeContents += `
-##&lt;${tagName}&gt;
-
-${element.desc}
-`;
-    }
-    for (const name in behaviorsByName) {
-      const behavior = behaviorsByName[name];
-
-      if (behavior.desc.trim() === '') {
-        readmeContents += `\n<!-- No docs for ${name} found. -->\n`;
-        continue;
-      }
-
-      readmeContents += `
-##${name}
-
-${behavior.desc}
-`;
-    }
-
-    const readmePath = path.join(element.dir, 'README.md')
-    const oldContents = fs.readFileSync(readmePath, 'utf8');
-    if (oldContents !== readmeContents) {
-      fs.writeFileSync(readmePath, readmeContents, 'utf8');
-      element.dirty = true;
-      return element.repo.createCommitOnHead(
-            ['README.md'], getSignature(), getSignature(),
-            'Autogenerate README file.');
-    }
-  });
-}
-
-let contributionGuideContents = null;
-function cleanupContributionGuide(element) {
-  const pathToCanonicalGuide = 'repos/ContributionGuide/CONTRIBUTING.md';
-  if (!existsSync(pathToCanonicalGuide)) {
-    return Promise.reject(new Error(
-        'Couldn\'t find canonical contribution guide. git checkout error?'));
-  }
-  if (!contributionGuideContents) {
-    contributionGuideContents = `
-<!--
-This file is autogenerated based on
-https://github.com/PolymerElements/ContributionGuide/blob/master/CONTRIBUTING.md
-
-If you edit that file, it will get updated everywhere else.
-If you edit this file, your changes will get overridden :)
--->
-`;
-    contributionGuideContents += fs.readFileSync(pathToCanonicalGuide, 'utf8')
-  }
-  let existingGuideContents = '';
-  const pathToExistingGuide = path.join(element.dir, 'CONTRIBUTING.md');
-  let guideExists = false;
-  if (existsSync(pathToExistingGuide)) {
-    guideExists = true;
-    existingGuideContents = fs.readFileSync(pathToExistingGuide, 'utf8');
-  }
-  if (contributionGuideContents === existingGuideContents) {
-    return Promise.resolve();
-  }
-  fs.writeFileSync(pathToExistingGuide, contributionGuideContents, 'utf8');
-  element.dirty = true;
-  let commitMessage = 'Update contribution guide';
-  if (!guideExists) {
-    commitMessage = 'Create contribution guide';
-  }
-  return element.repo.createCommitOnHead(
-        ['CONTRIBUTING.md'], getSignature(), getSignature(),
-        commitMessage);
-}
-
 // Call this as fast as you want, each time it returns a new promise that will
 // resolve only after a delay, and only after every other promise returned by
 // rateLimit has resolved.
 let rateLimit = (function() {
   let previousPromise = Promise.resolve();
   return function rateLimit(delay) {
-    let prev = previousPromise;
     let curr = previousPromise.then(function() {
       return new Promise((resolve) => {
         setTimeout(resolve, delay);
@@ -304,13 +89,13 @@ let rateLimit = (function() {
     });
     previousPromise = curr;
     return curr;
-  }
-})()
+  };
+})();
 
 
 function checkoutNewBranch(repo, branchName) {
   return repo.getHeadCommit().then((commit) => {
-    return nodegit.Branch.create(repo, branchName, commit, false)
+    return nodegit.Branch.create(repo, branchName, commit, false);
   }).then((branch) => {
     return repo.checkoutBranch(branch);
   });
@@ -341,7 +126,7 @@ function pushChanges(element, localBranchName, assignee) {
       .then(pushBranch.bind(null, element, localBranchName, remoteBranchName));
   if (element.needsReview) {
     return pushPromise.then(createPullRequest.bind(
-          null, element, branchName, 'master', assignee))
+          null, element, localBranchName, 'master', assignee));
   }
   return pushPromise;
 }
@@ -360,8 +145,6 @@ function pushBranch(element, localBranchName, remoteBranchName) {
           }
         }
       );
-
-      return remote.connect(nodegit.Enums.DIRECTION.PUSH);
     });
 }
 
@@ -399,7 +182,6 @@ function connectToGithub() {
 }
 
 
-const github = connectToGithub();
 
 let user;
 
@@ -412,7 +194,7 @@ Promise.resolve().then(() => {
   fs.mkdirSync('repos');
 }).then(() => {
   // We're going to need the github user later, better get it now.
-  return promisify(github.user.get)({})
+  return promisify(github.user.get)({});
 }).then((userResponse) => {
   user = userResponse;
 }).then(() => {
@@ -422,9 +204,9 @@ Promise.resolve().then(() => {
 
   for (const ghRepo of ghRepos) {
     promises.push(rateLimit(100).then(() => {
-      const targetDir = path.join('repos', ghRepo.name)
+      const targetDir = path.join('repos', ghRepo.name);
       let repoPromise;
-      if (existsSync(targetDir)) {
+      if (cleanup.existsSync(targetDir)) {
         repoPromise = nodegit.Repository.open(targetDir);
       } else {
         repoPromise = nodegit.Clone.clone(ghRepo.clone_url, targetDir, null);
@@ -468,7 +250,7 @@ Promise.resolve().then(() => {
   if (elementsPushed === 0) {
     console.log('No changes needed!');
   } else {
-    console.log(`Successfully pushed to ${elementsPushed} repos.`)
+    console.log(`Successfully pushed to ${elementsPushed} repos.`);
   }
 }, function(err) {
   console.error(`\n\n${err}`);
