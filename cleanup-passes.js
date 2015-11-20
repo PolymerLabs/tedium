707cc4ebfd666bdb76bba9c0d7f2f1361130c052
Daniel Freedman <dfreedm@google.com>
2015-11-19 17:33:20 -0800

    Make sure travis tests have polylint running
    Reorder the `install` and `run` steps for polylint if necessary

--- cleanup-passes.js
+++ cleanup-passes.js
@@ -298,7 +298,31 @@ function cleanupTravisConfig(element) {
 
   let travis = yaml.safeLoad(travisConfigBlob);
 
+  const installStep = 'npm install polylint';
+  const runStep = 'polylint';
+
   // update travis config
+  // Add polylint to all elements
+  if (Array.isArray(travis.before_script)) {
+    const bs = travis.before_script;
+    const installIndex = bs.indexOf(installStep);
+    const runIndex = bs.indexOf(runStep);
+    if (installIndex < 0 && runIndex < 0) {
+      // add both steps
+      bs.push(installStep);
+      bs.push(runStep);
+    } else if (runIndex >= 0 && installIndex < 0) {
+      // add install step before run step
+      bs.splice(runIndex, 0, installStep);
+    } else if (runIndex < 0 && installIndex >= 0) {
+      // add run step after install step
+      bs.push(runStep);
+    } else if (runIndex < installIndex) {
+      // reorder install step to be before run step
+      bs.splice(installIndex, 1);
+      bs.splice(runIndex, 0, installStep);
+    }
+  }
 
   const updatedTravisConfigBlob = yaml.safeDump(travis);
 
