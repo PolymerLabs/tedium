3f901ade9fc9133d8d8cef03a08b8ad7d2ead889
Daniel Freedman <dfreedm@google.com>
2015-12-10 15:06:48 -0800

    Fix WCT install
    Add C++11 compiler so `npm install` stops complaining
    Normalize `before_script`
    Use Node 5

--- cleanup-passes/travis.ts
+++ cleanup-passes/travis.ts
@@ -20,7 +20,6 @@ import * as path from 'path';
 import {ElementRepo} from '../element-repo';
 import {existsSync, makeCommit} from './util';
 
-
 async function cleanupTravisConfig(element: ElementRepo): Promise<void> {
   const travisConfigPath = path.join(element.dir, '.travis.yml');
 
@@ -32,56 +31,133 @@ async function cleanupTravisConfig(element: ElementRepo): Promise<void> {
 
   let travis = yaml.safeLoad(travisConfigBlob);
 
-  let needsReview = false;
+  // a small state class to represent the decision to make a PR or a commit to head
+  // Once tripped, a PR will be made
+  class Reviewer {
+    private _needsReview = false
+    get needsReview() {
+      return this._needsReview;
+    }
+    set needsReview(value: boolean) {
+      if (!this._needsReview) {
+        this._needsReview = value;
+      }
+    }
+  }
+
+  const review = new Reviewer();
+
+  const tools: string[] = [
+    'bower',
+    'polylint',
+    'web-component-tester'
+  ];
+
+  const beforeScript: string[] = [
+    `npm install -g ${tools.join(" ")}`,
+    'bower install',
+    'polylint'
+  ];
 
   // update travis config
   // Add polylint to all elements
   if (Array.isArray(travis.before_script)) {
-    const installStep = 'npm install polylint';
-    const runStep = 'polylint';
-
-    // assume we need a review
-    needsReview = true;
-
+    // assume we need review
     const bs: string[] = travis.before_script;
-    const installIndex = bs.indexOf(installStep);
-    const runIndex = bs.indexOf(runStep);
-
-    if (installIndex < 0 && runIndex < 0) {
-      // add both steps
-      bs.push(installStep);
-      bs.push(runStep);
-    } else if (runIndex >= 0 && installIndex < 0) {
-      // add install step before run step
-      bs.splice(runIndex, 0, installStep);
-    } else if (runIndex < 0 && installIndex >= 0) {
-      // add run step after install step
-      bs.push(runStep);
-    } else if (runIndex < installIndex) {
-      // reorder install step to be before run step
-      bs.splice(installIndex, 1);
-      bs.splice(runIndex, 0, installStep);
+    if (bs.length !== beforeScript.length) {
+      travis.before_script = beforeScript;
+      review.needsReview = true;
     } else {
-      // all is well
-      needsReview = false;
+      for (let i = 0; i < bs.length; i++) {
+        if (bs[i] !== beforeScript[i]) {
+          travis.before_script = beforeScript;
+          review.needsReview = true;
+          break;
+        }
+      }
     }
   }
 
-  // update travis config
+  // use stable node (v5+)
+
+  if (travis.node_js !== 'stable') {
+    travis.node_js = 'stable';
+  }
+
+  // travis addons
+
+  if (!travis.addons) {
+    travis.addons = {};
+  }
+  const ta = travis.addons;
+
+  //use latest firefox
+
+  if (!ta.firefox) {
+    ta.firefox = 'latest';
+  }
+
+  // use sauce addon to speed up tunnel creation
+  if (!ta.sauce_connect) {
+    ta.sauce_connect = true;
+  }
+
+  // update node >= 4 dependencies
+  // https://docs.travis-ci.com/user/languages/javascript-with-nodejs#Node.js-v4-(or-io.js-v3)-compiler-requirements
+
+  const C11Source = 'ubuntu-toolchain-r-test';
+  const C11Package = 'g++-4.8';
+  const C11Env = 'CXX=g++-4.8';
+
+  if (!ta.apt) {
+    ta.apt = {
+      sources: [],
+      packages: []
+    }
+  }
+
+  if (ta.apt.sources.indexOf(C11Source) === -1) {
+    ta.apt.sources.push(C11Source);
+  }
+  if (ta.apt.packages.indexOf(C11Package) === -1) {
+    ta.apt.packages.push(C11Package);
+  }
+
+  // use stable chrome
+  const ChromeSource = 'google-chrome';
+  const ChromePackage = 'google-chrome-stable';
+  if (ta.apt.sources.indexOf(ChromeSource) === -1) {
+    ta.apt.sources.push(ChromeSource);
+  }
+  if (ta.apt.packages.indexOf(ChromePackage) === -1) {
+    ta.apt.packages.push(ChromePackage);
+  }
+
+  // travis env
+  let te = travis.env;
+  if (!te) {
+    te = {global: []};
+  } else if (Array.isArray(te)) {
+    te = {global: te};
+  }
+
+  if (te.global.indexOf(C11Env) === -1) {
+    te.global.push(C11Env);
+  }
+
+  travis.env = te;
 
   const updatedTravisConfigBlob = yaml.safeDump(travis);
 
   if (travisConfigBlob !== updatedTravisConfigBlob) {
     fs.writeFileSync(travisConfigPath, updatedTravisConfigBlob, 'utf8');
-    element.needsReview = needsReview;
+    element.needsReview = review.needsReview;
     // if this commit needs review, run the tests
     // otherwise this is probably an innocuous run
     const commitMessage =
-        `${!needsReview ? '[ci skip] ' : ''}Update travis config`;
+        `${!review.needsReview ? '[ci skip] ' : ''}Update travis config`;
     await makeCommit(element, ['.travis.yml'], commitMessage);
   }
 }
-
-
 
 export let cleanupPasses = [cleanupTravisConfig];
